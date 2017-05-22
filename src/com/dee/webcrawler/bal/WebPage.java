40378d8ab74b26295dcd4adc3dd44f72d870214e
ThatsD <zhzago@hotmail.com>
2017-05-22 17:50:12 +0800

    Add SocketException handler to jsoup connect
    To avoid the TCP reset from website.
    Method should works to reconnect to the website when the server response with a TCP reset to the client

--- src/com/dee/webcrawler/bal/WebPage.java
+++ src/com/dee/webcrawler/bal/WebPage.java
@@ -1,6 +1,7 @@
 package com.dee.webcrawler.bal;
 
 import java.io.IOException;
+import java.net.SocketException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -18,11 +19,15 @@ public class WebPage {
 	private int _emailParseStatus;
 	private Document _document;
 	
+	private int _socketExpCounter;
+	
 	public WebPage(Anchor anchor) throws Exception{
 		_anchor = anchor;
 		_webPageHash = Hasher.toSha256(anchor.GetAnchorHash() + CommonBal.GetTimestamp().toString());
 		_anchorParseStatus = 0;
 		_emailParseStatus = 0;
+		
+		_socketExpCounter = 0;
 	}
 	
 	public String GetWebPageHash(){
@@ -47,18 +52,32 @@ public class WebPage {
 	public void loadDocumentFromWeb(){
 		
 		try {
-			_document = Jsoup.connect(_anchor.GetAnchorUrl()).get();
+			try{
+				_document = Jsoup.connect(_anchor.GetAnchorUrl()).get();
+			}catch (SocketException se){ //handle socket exception, may cuz caused by TCP reset
+				_socketExpCounter++;
+				System.out.println("\nSocketException! Times: " + _socketExpCounter);
+				se.printStackTrace();
+				try {
+					Thread.sleep(10000);
+					
+					loadDocumentFromWeb();
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
 			
 		} catch (IOException e) {
 			Logger.getLogger(WebPage.class.getName()).log(Level.SEVERE, null, e);
-		}
+		} 
 	}
 	
 	public String getTextFrom(){
 		Elements docas = _document.select("meta");
 		String targetText = null;
 		
-		System.out.println("Elements scaning!");
+		//System.out.println("Elements scaning!");
 		for(Element e : docas){
 			if(e.attr("name").toString().equals("description")){
 				targetText = e.attr("content").toString();
